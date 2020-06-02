e36f3307cd7e47850f44b8f3feb2b202daad05b6
Quentin Stoyel <quentin.stoyel@mail.mcgill.ca>
2020-06-02 12:13:34 -0300

    add plot maker

---
+++ DHT22_reader/get_data.py
@@ -0,0 +1,40 @@
+import sqlite3
+import sys
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+db_filename = "apartment.db"
+def get_data():
+	#queries db
+	con = sqlite3.connect(db_filename)
+	data = pd.read_sql_query("SELECT * FROM apartment", con)
+	con.close()
+	data["timestamp"] = data["timestamp"].apply(string_to_datetime)
+	now = datetime.datetime.now()
+	hour_ago = now - datetime.timedelta(hours=1)
+	day_ago = now - datetime.timedelta(days=1)
+	week_ago = now - datetime.timedelta(days = 7)
+
+	#make and save plots
+	save_data_to_fig(data[data["timestamp"] > hour_ago, "hour"])
+	save_data_to_fig(data[data["timestamp"] > day_ago], "day")
+	save_data_to_fig(data[data["timestamp"] > week_ago], "week")	
+	return 0
+
+def save_data_to_fig(data, n):
+	plt.figure(figsize=(12,6))
+	plt.plot(data["timestamp"], data["hum"])
+	plt.title("Humidity")
+	plt.ylabel("Humidity (%)")
+	plt.xlabel("Time")
+	plt.savefig("tmp/hum_"+n+".png")
+	plt.figure(figsize=(12,6))
+	plt.plot(data["timestamp"], data["temp"])
+	plt.title("Temperature")
+	plt.xlabel("Time")
+	plt.ylabel("Temperature (C)")
+	plt.savefig("tmp/temp_"+n+".png")
+
+def string_to_datetime(timestr):
+		return datetime.datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S')
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from DHT22_reader.get_data import get_data
+
 
 app = Flask(__name__)
 
@@ -8,5 +10,12 @@ def index():
 	
 @app.route("/apartment")
 def apartment():
+	get_data() #queries db, generates plots
 	return render_template("apartment.html")
+	
+
+def clean_data():
+	return True
+
+clean_data() #empties tmp file
 	
